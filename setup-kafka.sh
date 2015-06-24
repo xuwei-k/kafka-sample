89a48bc600a4bf1a27bcd467c053ac43534c59c6
xuwei-k <6b656e6a69@gmail.com>
2015-06-25 02:56:58 +0900

    kafka 0.8.2.1

--- setup-kafka.sh
+++ setup-kafka.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-KAFKA="kafka_2.11-0.8.2.0"
+KAFKA="kafka_2.11-0.8.2.1"
 
 if [ ! -d "kafka-install/$KAFKA" ]; then
   mkdir -p kafka-install &&
   cd kafka-install &&
-  curl -L http://www.us.apache.org/dist/kafka/0.8.2.0/$KAFKA.tgz | tar xvz
+  curl -L http://www.us.apache.org/dist/kafka/0.8.2.1/$KAFKA.tgz | tar xvz
 fi
 
